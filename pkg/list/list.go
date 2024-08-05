316acaf1deec103745fa3ebe5478d6390d8525b6
Eric Wohltman <eric.wohltman@gmail.com>
2024-08-05 06:35:42 -0400

    Initial commit

---
+++ pkg/list/list.go
@@ -0,0 +1,96 @@
+package list
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+const (
+	listHeight   = 14
+	defaultWidth = 20
+)
+
+var (
+	titleStyle      = lipgloss.NewStyle().MarginLeft(2)
+	paginationStyle = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle       = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	quitTextStyle   = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+)
+
+type Model struct {
+	l        list.Model
+	choice   string
+	quitting bool
+}
+
+func NewModel() Model {
+	items := []list.Item{
+		Item("Ramen"),
+		Item("Tomato Soup"),
+		Item("Hamburgers"),
+		Item("Cheeseburgers"),
+		Item("Currywurst"),
+		Item("Okonomiyaki"),
+		Item("Pasta"),
+		Item("Fillet Mignon"),
+		Item("Caviar"),
+		Item("Just Wine"),
+	}
+
+	l := list.New(items, ItemDelegate{}, defaultWidth, listHeight)
+	l.Title = "What do you want for dinner?"
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(false)
+	l.Styles.Title = titleStyle
+	l.Styles.PaginationStyle = paginationStyle
+	l.Styles.HelpStyle = helpStyle
+
+	return Model{l: l}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.l.SetWidth(msg.Width)
+		return m, nil
+
+	case tea.KeyMsg:
+		switch keypress := msg.String(); keypress {
+		case "q", "ctrl+c":
+			m.quitting = true
+			return m, tea.Quit
+
+		case "enter":
+			i, ok := m.l.SelectedItem().(Item)
+			if ok {
+				m.choice = string(i)
+			}
+			return m, tea.Quit
+		}
+	}
+
+	var cmd tea.Cmd
+
+	m.l, cmd = m.l.Update(msg)
+
+	return m, cmd
+}
+
+func (m Model) View() string {
+	if m.choice != "" {
+		return quitTextStyle.Render(fmt.Sprintf("%s? Sounds good to me.", m.choice))
+	}
+
+	if m.quitting {
+		return quitTextStyle.Render("Not hungry? That’s cool.")
+	}
+
+	return "\n" + m.l.View()
+}
--- pkg/model/model.go
+++ pkg/model/model.go
@@ -2,97 +2,47 @@ package model
 
 import (
 	"fmt"
-	"io"
-	"strings"
 
-	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+
+	"github.com/ewohltman/charming/pkg/list"
+	"github.com/ewohltman/charming/pkg/viewport"
 )
-
-const ListHeight = 14
-
-var (
-	TitleStyle        = lipgloss.NewStyle().MarginLeft(2)
-	ItemStyle         = lipgloss.NewStyle().PaddingLeft(4)
-	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
-	PaginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
-	HelpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
-	QuitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
-)
-
-type Item string
-
-func (i Item) FilterValue() string { return "" }
-
-type ItemDelegate struct{}
-
-func (d ItemDelegate) Height() int { return 1 }
-
-func (d ItemDelegate) Spacing() int { return 0 }
-
-func (d ItemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
-
-func (d ItemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
-	i, ok := listItem.(Item)
-	if !ok {
-		return
-	}
-
-	str := fmt.Sprintf("%d. %s", index+1, i)
-
-	fn := ItemStyle.Render
-	if index == m.Index() {
-		fn = func(s ...string) string {
-			return selectedItemStyle.Render("> " + strings.Join(s, " "))
-		}
-	}
-
-	fmt.Fprint(w, fn(str))
-}
 
 type Model struct {
-	List     list.Model
-	choice   string
-	quitting bool
+	listModel     list.Model
+	viewportModel viewport.Model
+}
+
+func NewModel() Model {
+	return Model{
+		listModel:     list.NewModel(),
+		viewportModel: viewport.NewModel(),
+	}
 }
 
 func (m Model) Init() tea.Cmd {
-	return nil
+	return tea.Batch(
+		m.listModel.Init(),
+	)
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.List.SetWidth(msg.Width)
-		return m, nil
+	listModel, listCmd := m.listModel.Update(msg)
+	viewportModel, viewportCmd := m.viewportModel.Update(msg)
 
-	case tea.KeyMsg:
-		switch keypress := msg.String(); keypress {
-		case "q", "ctrl+c":
-			m.quitting = true
-			return m, tea.Quit
+	m.listModel = listModel.(list.Model)
+	m.viewportModel = viewportModel.(viewport.Model)
 
-		case "enter":
-			i, ok := m.List.SelectedItem().(Item)
-			if ok {
-				m.choice = string(i)
-			}
-			return m, tea.Quit
-		}
-	}
-
-	var cmd tea.Cmd
-	m.List, cmd = m.List.Update(msg)
-	return m, cmd
+	return m, tea.Batch(
+		listCmd,
+		viewportCmd,
+	)
 }
 
 func (m Model) View() string {
-	if m.choice != "" {
-		return QuitTextStyle.Render(fmt.Sprintf("%s? Sounds good to me.", m.choice))
-	}
-	if m.quitting {
-		return QuitTextStyle.Render("Not hungry? That’s cool.")
-	}
-	return "\n" + m.List.View()
+	return fmt.Sprintf("%s\n%s\n",
+		m.listModel.View(),
+		m.viewportModel.View(),
+	)
 }
